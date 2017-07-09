bea27d9f8da9e9f989d8bf661e7dc648d420625c
RIS_CL3 <RIS_CL3@192.168.251.5>
2017-07-09 13:33:18 +0530

    Forth Commit

--- src/GetDistData/DBConncection.java
+++ src/GetDistData/DBConncection.java
@@ -7,12 +7,14 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 
 public class DBConncection {
-    
+    public static void main(String[] args) {
+        PrimaryConnection();
+    }
        public static void PrimaryConnection(){
         String userName="us3r";
         String pass="pass";
         String driverUrl=null;
-        String Connurl="jdbc:derby:F:\\db\\java\\.db\\DistData";
+        String Connurl="jdbc:derby:\\DistData;create=true";//E:\\db\\java\\.db
         PreparedStatement pstmt = null;
         String sqlQuey="INSERT INTO";
         try (
