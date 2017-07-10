937e84b1dfee2f34f55333b9f03f9c53aae4600e
Arnab <Arnab@Home>
2017-07-11 00:30:57 +0530

    Completed_fail, writing data in .txt.

--- src/GetDistData/JsoupURLTester.java
+++ src/GetDistData/JsoupURLTester.java
@@ -1,11 +1,14 @@
 package GetDistData;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
@@ -23,72 +26,73 @@ public class JsoupURLTester {
     private static Document doc;
 
     public static void main(String[] args) {
-        String sqlQuery = "INSERT INTO"+
-                "US3R.DIST_DATA (DIST_NAME,PROP_NAME,MOB,LANDLINE,EMAIL_AO_NAME)"+
-                "VALUE (?,?,?,?,?,?);";
-        
-        for (int counter = 1081;; counter++) {
-
-            //int il = 2;
-            String websiteUrl = "https://indane.co.in/distributor_details.php?distid=1" + counter;
-
+        //String query = null;
+        String Connurl = "jdbc:derby:src/db/DistData;create=true";
+        String userName = "us3r";
+        String pass = "pass";
+        String Url = "https://indane.co.in/distributor_details.php?distid=";
+        String webSite=null;
+        int counter = 1081;
+        try (PrintWriter writer = new PrintWriter("DistList.txt", "UTF-8")) {
+            for (;; counter++) {
             try {
-                doc = Jsoup.connect(websiteUrl)//connection @Arnab
+                
+            
+            
+
+            webSite=Url+counter;
+            // + counter;
+//            File file = new File("C:/Users/Arnab/Desktop/index.html");
+            
+//                doc = Jsoup.parse(file, "utf-8", "abcd.co.in");
+                doc = Jsoup.connect(webSite)//connection @Arnab
                         .userAgent("Mozilla/5.0 ")
                         .timeout(600)
                         .get();
-                System.out.println("Connected");              
-            } catch (IOException ex) {
-                System.err.println(ex);
-            }
-            // tree selection,05/07/17,
-            System.out.println("\n" + counter + "\n");
-            Element tableOne = doc.select("table").get(1);
-            Elements rows = tableOne.select("tr");
-            String distName = tableOne.select("div").get(0).text();
+                System.out.println("\n" + counter + "\n");
+                Element tableOne = doc.select("table").get(1);
+                Elements rows = tableOne.select("tr");
+                String distName = tableOne.select("div").get(0).text();
 
-            Element distPropName = tableOne.select("div").get(3);
-            String s1 = distPropName.select("div").get(1).text();
-            String PropName = s1.substring(21);
+                Element distPropName = tableOne.select("div").get(3);
+                String s1 = distPropName.select("div").get(1).text();
+                String PropName = s1.substring(21);
 
-            Element tableTwo = doc.select("table").get(2);
-            String LandlindNo = tableTwo.select("td").get(2).text();
-            Element tableThree = doc.select("table").get(3);
-            String MobileNo = tableTwo.select("td").get(4).text();
-            String eMail = tableTwo.select("td").get(6).text();
+                Element tableTwo = doc.select("table").get(2);
+                String LandlindNo = tableTwo.select("td").get(2).text();
+                Element tableThree = doc.select("table").get(3);
+                String MobileNo = tableTwo.select("td").get(4).text();
+                String eMail = tableTwo.select("td").get(6).text();
 
-            Element tableFour = doc.select("table").get(4);
-            String AO_Name = tableFour.select("td").get(9).text();
+                Element tableFour = doc.select("table").get(4);
+                String AO_Name = tableFour.select("td").get(9).text();
 
-            //Ouputing Data 
-            System.out.println(distName);
-            System.out.print(PropName + "\n");//Have to use Substring class for removing the "name of proprietor
-            System.out.print(LandlindNo + "\n");
-            System.out.print(MobileNo + "\n");
-            System.out.print(eMail + "\n");
-            System.out.print(AO_Name + "\n");
-
-            System.out.println("\n" + counter + "\n");
-            try (Connection conn = DBConncection.getConnection();
-                PreparedStatement prst=conn
-                        .prepareStatement(sqlQuery,Statement.RETURN_GENERATED_KEYS);) {
                 
-                prst.setString(1,distName );
-                prst.setString(2,PropName );
-                prst.setString(3,LandlindNo );
-                prst.setString(4,MobileNo );
-                prst.setString(5,eMail );
-                prst.setString(6,AO_Name );
+                    writer.println("\n"+
+                            +counter+
+                            ","
+                            +distName+
+                            ","
+                            +PropName+
+                            ","
+                            +LandlindNo+
+                            ","
+                            +MobileNo+
+                            ","
+                            +eMail+
+                            ","
+                            +AO_Name+"\n");
                 
-                int affected = prst.executeUpdate();
-                if(affected==1){
-                    System.out.println("Sucessfully Inserted");
-                } else {
-                    System.out.println("Fiald to insert");
+                } catch (Exception e) {
+            e.printStackTrace();
                 }
-               
-            } catch (Exception e) {
+            
             }
-        }
-    }
+            }catch (IOException ex) {
+               System.err.println(ex);
+              
+            }
+
+
+}
 }
