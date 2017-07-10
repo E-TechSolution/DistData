71cb716b74c10893f27ee734ca1816c4ff39f857
Arnab <Arnab@Home>
2017-07-10 19:06:29 +0530

    After Changing Variable Name for adding into DB

--- src/GetDistData/JsoupURLTester.java
+++ src/GetDistData/JsoupURLTester.java
@@ -15,53 +15,52 @@ import org.jsoup.select.Elements;
 public class JsoupURLTester {
 
     private static Document doc;
+
     public static void main(String[] args) {
-        
-    }
-    
-   private static void GetData() {
-        for (int i = 1081; ;i++) {
-            
+
+        for (int counter = 1081;; counter++) {
+
             //int il = 2;
-        String websiteUrl="https://indane.co.in/distributor_details.php?distid=1"+i;
-            
-        
-        try {
-            doc = Jsoup.connect(websiteUrl)//connection @Arnab
-             .userAgent("Mozilla/5.0 ")
-             .timeout(600)
-             .get();
-            System.out.println("Connected");
-        } catch (IOException ex) {
-            System.err.println(ex);
+            String websiteUrl = "https://indane.co.in/distributor_details.php?distid=1" + counter;
+
+            try {
+                doc = Jsoup.connect(websiteUrl)//connection @Arnab
+                        .userAgent("Mozilla/5.0 ")
+                        .timeout(600)
+                        .get();
+                System.out.println("Connected");
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+            // tree selection,05/07/17,
+            System.out.println("\n" + counter + "\n");
+            Element tableOne = doc.select("table").get(1);
+            Elements rows = tableOne.select("tr");
+            String distName = tableOne.select("div").get(0).text();
+
+            Element distPropName = tableOne.select("div").get(3);
+            String s1 = distPropName.select("div").get(1).text();
+            String PropName = s1.substring(21);
+
+            Element tableTwo = doc.select("table").get(2);
+            String LandlindNo = tableTwo.select("td").get(2).text();
+            Element tableThree = doc.select("table").get(3);
+            String MobileNo = tableTwo.select("td").get(4).text();
+            String eMail = tableTwo.select("td").get(6).text();
+
+            Element tableFour = doc.select("table").get(4);
+            String AO_Name = tableFour.select("td").get(9).text();
+
+            //Ouputing Data 
+            System.out.println(distName);
+            System.out.print(PropName + "\n");//Have to use Substring class for removing the "name of proprietor
+            System.out.print(LandlindNo + "\n");
+            System.out.print(MobileNo + "\n");
+            System.out.print(eMail + "\n");
+            System.out.print(AO_Name + "\n");
+
+            System.out.println("\n" + counter + "\n");
+
         }
-        // tree selection,05/07/17,
-         System.out.println("\n"+i+"\n");
-         Element tableOne = doc.select("table").get(1);
-         Elements rows = tableOne.select("tr");
-         String distName = tableOne.select("div").get(0).text();
-         System.out.println(distName);
-         Element distPropName = tableOne.select("div").get(3);
-         String s1 = distPropName.select("div").get(1).text();
-         String distPropNameOne =s1.substring(21);
-         System.out.print(distPropNameOne+"\n");//Have to use Substring class for removing the "name of proprietor
-        
-         Element tableTwo = doc.select("table").get(2);
-         String rowTwo=tableTwo.select("td").get(2).text();
-         Element tableThree = doc.select("table").get(3);
-         String rowThrree=tableTwo.select("td").get(4).text();
-         String rowFour=tableTwo.select("td").get(6).text();
-         System.out.print(rowTwo+"\n");
-         System.out.print(rowThrree+"\n");
-         System.out.print(rowFour+"\n");
-          //Element tableFour = doc.select("table").get(5);
-         Element tableFour = doc.select("table").get(4);
-          String rowFive= tableFour.select("td").get(9).text();
-         System.out.print(rowFive+"\n");
-
-         System.out.println("\n"+i+"\n");        
-
-    
-}
-}
+    }
 }
